9f6c75454810ee0fe8c4c4d31d91871c631dc9bd
Padma-Bhavanasi <padma.bhavanasi@freshworks.com>
2024-11-08 08:40:39 +0530

    Simple barcode scanner

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2a1b0a73cea16e45945da3b9e82f5c80",
+const RESOURCES = {"flutter_bootstrap.js": "af0839608606618ef4950cd3eec1a96e",
 "version.json": "58c46ce6dfad8b4c8ea398670f7aad23",
 "index.html": "32980749eecae7860e2f710e2a83f0e9",
 "/": "32980749eecae7860e2f710e2a83f0e9",
-"main.dart.js": "dc1746b6dbc19b67fa41d27a4f12a536",
+"main.dart.js": "138baf37dbb53544c07cf6880c40ff5f",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "6c5c5952fbfd64916140f531ef9b208b",
